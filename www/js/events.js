b71eeb3eeedc6aead55da0d6c05386cc2aaab7c2
3bedo <abdulla.hamam@carleton.ca>
2015-03-21 13:04:43 -0400

    uncommited changes

--- www/js/events.js
+++ www/js/events.js
@@ -15,7 +15,7 @@ var events = {
             console.log(JSON.stringify(response));
             room.id = response.record._id;
             window.localStorage.setItem('events',JSON.stringify(events.list));
-            events.updateUI();
+            events.load();
             nav.goTo('events_page',false);
         }, function(error){
             console.log(JSON.stringify(error));
